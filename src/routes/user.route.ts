4a692c3ee37c3a1dbca4d9cae0c37598b3b43a05
Kevin Nimi <k.nimi73@gmail.com>
2024-12-28 19:59:42 -0500

    Update

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,10 @@
-import { register, validateCode } from "../controllers/user";
+import { login, register, validateCode } from "../controllers/user";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/validate", validateCode);
+router.post("login", login);
 
 export default router;
